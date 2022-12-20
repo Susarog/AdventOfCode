4f70413ffe192bcda430d89a1d1f019985ee533d
Ryan Hang <ryanhang92802@gmail.com>
2022-12-19 17:04:02 -0800

    finish day5

---
+++ day5/part2.ts
@@ -0,0 +1,39 @@
+import * as fs from 'fs';
+
+const data = fs.readFileSync('./input.txt', 'utf-8');
+const arrayOfStacks: Array<Array<string>> = [];
+for (let i = 0; i < 10; i++) {
+  arrayOfStacks.push([]);
+}
+arrayOfStacks[1].push('Q', 'M', 'G', 'C', 'L');
+arrayOfStacks[2].push('R', 'D', 'L', 'C', 'T', 'F', 'H', 'G');
+arrayOfStacks[3].push('V', 'J', 'F', 'N', 'M', 'T', 'W', 'R');
+arrayOfStacks[4].push('J', 'F', 'D', 'V', 'Q', 'P');
+arrayOfStacks[5].push('N', 'F', 'M', 'S', 'L', 'B', 'T');
+arrayOfStacks[6].push('R', 'N', 'V', 'H', 'C', 'D', 'P');
+arrayOfStacks[7].push('H', 'C', 'T');
+arrayOfStacks[8].push('G', 'S', 'J', 'V', 'Z', 'N', 'H', 'P');
+arrayOfStacks[9].push('Z', 'F', 'H', 'G');
+
+data
+  .trim()
+  .split('\n')
+  .forEach((input) => {
+    const arrInput = input.split(' ');
+    const amountToMove = Number(arrInput[1]);
+    const from = Number(arrInput[3]);
+    const to = Number(arrInput[5]);
+    const temp = arrayOfStacks[from].slice(
+      arrayOfStacks[from].length - amountToMove,
+      arrayOfStacks[from].length
+    );
+    arrayOfStacks[from] = arrayOfStacks[from].slice(
+      0,
+      arrayOfStacks[from].length - amountToMove
+    );
+    arrayOfStacks[to].push(...temp);
+  });
+
+for (let i = 0; i < arrayOfStacks.length; i++) {
+  console.log(arrayOfStacks[i].pop());
+}
